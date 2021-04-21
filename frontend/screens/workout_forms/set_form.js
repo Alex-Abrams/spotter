c7a2ac6d1b082138539f600046c6adc4635aa159
Alex-Abrams <alexander.abrams99@gmail.com>
2021-04-21 16:58:27 -0700

    Deleted redundant file.  Created update form functions to only accept numbers as input

--- frontend/screens/workout_forms/set_form.js
+++ frontend/screens/workout_forms/set_form.js
@@ -8,14 +8,53 @@ class SetForm extends React.Component {
     super(props);
 
     this.state = {
-
+      weight: '',
+      reps: '',
+      onlyNumeric: null,
+      weightNumeric: null,
+      repsNumeric: null,
     };
   }
   // this basically will have the 2 inputs for reps(repetitions) and weight
   // two inputs, and a "submit form...ish" type of button to solidify the sets
   // once the button is entered either have a button for adding set or just make the next input form visible
 
+  submitForm() {
+    // state sends to store
+  }
+
+  updateForm(input, section) {
+    // const letters = /^[A-Za-z]+$/;
+    // const numbers = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ."];
+    const num = /^[0-9]+$/;
+    if (input.match(num)) {
+      // this.setState({weight: input});
+      if (section === "weight") {
+        this.setState({ weight: input, weightNumeric: true });
+      } else {
+        this.setState({ reps: input, repsNumeric: true });
+      };
+
+      // this.setState({ onlyNumeric: true });
+      // console.log("bingo", this.state.onlyNumeric);
+    } else {
+      if (section === "weight") {
+        this.setState({ weight: input, weightNumeric: false });
+      } else {
+        this.setState({ reps: input, repsNumeric: false });
+      };
+      // this.setState({onlyNumeric: false});
+      // this.setState({weight: input});
+      // console.log("not a fucking number or .", this.state.onlyNumeric);
+    };
+
+    // this.setState({weight: input});
+  }
+
   render() {
+    console.log("after render", this.state.onlyNumeric);
+    console.log(this.state);
+    // console.log("this.state.weight", this.state.weight);
     return(
       <View style={{paddingLeft: 16, paddingRight: 16}}>
         <Text>Set # oi m8</Text>
@@ -23,6 +62,8 @@ class SetForm extends React.Component {
       <View style={{ paddingTop: 8 }}>
         <TextInput
           placeholder="weight"
+          onChangeText={input => this.updateForm(input, "weight")}
+          value={this.state.weight}
           style={{borderWidth: 1, width: "30%", height: 32}}
           />
       </View>
@@ -30,6 +71,7 @@ class SetForm extends React.Component {
         <TextInput
         placeholder="# of reps"
         style={{borderWidth: 1, width: "30%", height: 32}}
+        onChangeText={input => this.updateForm(input, "reps")}
         />
       </View>
     </View>
