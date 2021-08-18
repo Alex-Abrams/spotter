e97ef7ee87ad2986bef968485fb4a5acaa9a375f
Alex-Abrams <alexander.abrams99@gmail.com>
2021-08-18 16:22:28 -0700

    Added react antive charts library and first chart set up.

--- frontend/screens/graph_screens/graph_screen_menu.js
+++ frontend/screens/graph_screens/graph_screen_menu.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 class GraphMenuScreen extends React.Component {
   constructor(props) {
@@ -9,7 +17,56 @@ class GraphMenuScreen extends React.Component {
   render() {
     return(
       <View>
-        <Text>GraphScreen MENUUUUU</Text>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  // Math.random() * 100,
+                  // Math.random() * 100,
+                  // Math.random() * 100,
+                  // Math.random() * 100,
+                  // Math.random() * 100,
+                  // Math.random() * 100
+                  100,
+                  120,
+                  170,
+                  190,
+                  250,
+                  300
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00", // back from and to the same to be same color background
+            backgroundGradientTo: "#ffa726", // or have it frad from bottom left to top right different colors
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(200, 200, 200, ${opacity})`, // color of the lline going through middle, slope
+            labelColor: (opacity = 1) => `rgba(100, 100, 100, ${opacity})`, // color of the x, y axis labels
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
       </View>
     );
   }
