a26bccd39073df7bd0b2126fe14b3d6e4f811b66
Alex-Abrams <alexander.abrams99@gmail.com>
2021-08-26 15:56:28 -0700

    Added new drop down library.  Added drop down select to charts

--- frontend/actions/chart_actions.js
+++ frontend/actions/chart_actions.js
@@ -1,6 +1,6 @@
 export const RECEIVE_CHART_WORKOUTS = "RECEIVE_CHART_WORKOUTS";
 export const RECEIVE_CHART_LIFTS = "RECEIVE_CHART_LIFTS";
-// export const RECEIVE_SINGLE_CHART_EXERCISE = "RECEIVE_SINGLE_CHART_EXERCISE";
+
 import { requestAllWorkouts, requestAllWorkoutExercises } from './prev_workout_actions';
 
 export const receiveChartWorkouts = (workouts) => ({
@@ -12,11 +12,6 @@ export const receiveChartExercises = (exercises) => ({
   type: RECEIVE_CHART_LIFTS,
   exercises
 });
-
-// export const recieveSingleChartExercises = (exercises) => ({
-//    type: RECEIVE_SINGLE_CHART_EXERCISE,
-//    exercise
-// });
 
 
 export function requestChartExercises(user_id, auth_token) {
@@ -38,34 +33,3 @@ export function requestChartExercises(user_id, auth_token) {
     );
   }
 }
-
-export function requestChartWorkouts(user_id, auth_token) { // need this but.. it could just loop
-  return function action(dispatch) {
-    const request = fetch(`http://10.0.2.2:3000/users/${user_id}/workouts`, {
-      method: 'GET',
-      headers: {
-        "Authorization": auth_token
-      }
-    });
-
-    return request.then(
-      response => response.json(),
-      err => console.log("errrr", err),
-    )
-    .then(
-      // json => dispatch(receiveChartWorkouts(json)),
-      json => {
-        let array = [];
-      Object.values(json).forEach(workout => array.push(workout.id));
-      // Object.values(json).forEach(workout => dispatch(requestChartExercises(user_id, workout.id, auth_token)));
-      dispatch(receiveChartWorkouts(array));
-      // requestChartExercises gets the exerccies for that workout \
-      // console.log(array);
-      // console.log(json);
-      },
-      err => console.log("chart workouts json error", err),
-    );
-  }
-}
-
-// should be a single recieve Exercies
--- frontend/reducers/charts_reducer.js
+++ frontend/reducers/charts_reducer.js
@@ -1,7 +1,6 @@
 import {
   RECEIVE_CHART_WORKOUTS,
   RECEIVE_CHART_LIFTS,
-  // RECEIVE_SINGLE_CHART_EXERCISE
 } from '../actions/chart_actions';
 
 import merge from 'lodash/merge';
@@ -10,10 +9,6 @@ const chartsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch(action.type) {
-    // case RECEIVE_SINGLE_CHART_EXERCISE:
-    //   return merge({}, state, { [action.exercise.id]: action.exercise })
-    case RECEIVE_CHART_WORKOUTS:
-      return merge({}, state, action.workouts);
     case RECEIVE_CHART_LIFTS:
       return merge({}, state, action.exercises);
     default:
