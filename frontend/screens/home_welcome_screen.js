529dd319de4f14572168318488abaea9840f2e39
Alex-Abrams <alexander.abrams99@gmail.com>
2021-12-30 19:30:12 -0800

    Added functions in welcome screen to display records if there is information in the store, or a message saying there are not records as of yet.

--- frontend/screens/home_welcome_screen.js
+++ frontend/screens/home_welcome_screen.js
@@ -14,11 +14,40 @@ class HomeWelcomeScreen extends React.Component {
     this.props.fetchAllExercises.requestChartExercises(current_user.id, auth_token);
   }
 
+  displayRecords(sorted_by_weight) {
+    // if there are no information it will display something other than the current records
+    const { current_user, all_exercises } = this.props;
+
+    const display_records_or_na = (all_exercises.length > 0) ? (
+      <View>
+        <Text>Heavy Compound Workout Records</Text>
+          {/* find the heaviest of each category and use whatever name is listed */}
+        <Text>Arms: {sorted_by_weight[0].name} {sorted_by_weight[0].weight}</Text>
+        <Text>Shoulders: {sorted_by_weight[1].name} {sorted_by_weight[1].weight}</Text>
+        <Text>Chest: {sorted_by_weight[2].name} {sorted_by_weight[2].weight}</Text>
+        <Text>Legs: {sorted_by_weight[3].name} {sorted_by_weight[3].weight}</Text>
+        <Text>Back: {sorted_by_weight[4].name} {sorted_by_weight[4].weight}</Text>
+      </View>
+    ) : (
+      <View>
+        <Text>No workouts yet</Text>
+        <Text>New Workout Records Show up there</Text>
+      </View>
+    );
+
+    return(
+      <View>
+        {display_records_or_na}
+      </View>
+    );
+
+  }
+
 
 
 
   render() {
-    const { current_user, auth_token, all_exercises } = this.props;
+    const { current_user, all_exercises } = this.props;
 
 
     const sorted_by_type = [];  // sorting all_exercises into an array of sectioned exercises in the order of the types variable
@@ -29,52 +58,36 @@ class HomeWelcomeScreen extends React.Component {
       sorted_by_type.push(type);
     };
 
-    // console.log('pisssss', sorted_by_type[0][0].weight > 0);
-
     // now i need to find the highest lbs workout for each catagory
 
     const sorted_by_weight = [];  // an array of the highest lbs object
-    // console.log(sorted_by_type[0].length);
+
     for(let i = 0; i < sorted_by_type.length; i ++) {  // go over the array of arrayed sections (5 times)
       let heaviest = 0;
-      if (i === 0) {console.log('ppppppp', heaviest)};
-      // console.log(heaviest);
+
       // next loop iterates through each array section
       for(let z = 0; z < sorted_by_type[i].length; z ++) {
         if (sorted_by_type[i][z].weight > heaviest) { // boolean replaces the highest current weight
           heaviest = sorted_by_type[i][z].weight;
-          sorted_by_weight[i] = heaviest; // sets the weight in order in the sorted_by_weight array
+          sorted_by_weight[i] = sorted_by_type[i][z]; // sets the weight in order in the sorted_by_weight array
         };
       };
       heaviest = 0;
 
     };
 
-    console.log('sorted_by_weight', sorted_by_weight);
-
 
     return(
       <ScrollView>
         <View>
           <Text>Welcome to Spotter!!</Text>
+          {/* something about welcome to spotter use the tabs on the left site to navigate screens, developed by Alexander Abrams */}
         </View>
 
+        {/* put a line here */}
 
-        <View>
-          <Text>Heavy Compound Workout Records</Text>
-            {/* find the heaviest of each category and use whatever name is listed */}
-          <Text></Text>
-          <Text></Text>
-          <Text></Text>
-          <Text></Text>
-          <Text></Text>
-        </View>
+        {this.displayRecords(sorted_by_weight)}
 
-
-        <View>
-          <Text>Spotlight Records</Text>
-          <Text>(No Results Yet!)</Text>
-        </View>
       </ScrollView>
     );
   }
