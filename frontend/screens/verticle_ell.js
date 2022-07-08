5b160adcef1e6f065a83bee9c70add55b45dd514
alex abrams <alexander.abrams99@gmail.com>
2022-07-08 16:28:59 -0700

    Completed the 3 dots and proper collring.

--- frontend/screens/verticle_ell.js
+++ frontend/screens/verticle_ell.js
@@ -9,13 +9,29 @@ class Ellipse extends React.Component {
 
   render() {
     return(
-    <View>
+    <View style={styles.dot_container}>
       <TouchableOpacity onPress={() => console.log('dah ellipse')}>
-        <Text></Text>
+        <View style={styles.dot}></View>
+        <View style={styles.dot}></View>
+        <View style={styles.dot}></View>
       </TouchableOpacity>
     </View>
-    )
+  );
   }
 }
+
+const styles = StyleSheet.create({
+  dot_container: {
+    paddingRight: 15,
+  },
+  dot: {
+    height: 7,
+    width: 7,
+    borderWidth: 4,
+    borderRadius: 40,
+    borderColor: '#9e9e9e',  // a darkish gray
+    marginTop: 2,
+  }
+});
 
 export default Ellipse;
